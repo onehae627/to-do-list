9aea72b70e368ef2878510bd998bf822683c9425
onehae627 <onehae627@gmail.com>
2023-02-15 16:04:13 +0900

    할 일 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { AppBar, Toolbar } from "@mui/material";
-import { Routes, Navigate, Route, useLocation, NavLink } from "react-router-dom";
+import {
+  Routes,
+  Navigate,
+  Route,
+  useLocation,
+  NavLink,
+} from "react-router-dom";
 
 import MainPage from "./pages/MainPage";
 import WritePage from "./pages/WritePage";
@@ -9,25 +15,31 @@ function App() {
   const location = useLocation();
   return (
     <>
-    <AppBar position="static">
+      <AppBar position="static">
         <Toolbar>
           <div className="flex-1"></div>
           <span className="font-bold select-none">앱 이름</span>
           <div className="flex-1 flex justify-end">
             {/* 메인일때는 서브1 보이게. */}
-            {location.pathname != "/write" && <NavLink to="/write" className="select-none">글쓰기</NavLink>}
+            {location.pathname != "/write" && (
+              <NavLink to="/write" className="select-none">
+                글쓰기
+              </NavLink>
+            )}
             {/* 서브1일때는 메인 보이게. */}
-            {location.pathname == "/write" && <NavLink to="/main" className="select-none">메인</NavLink>}
+            {location.pathname == "/write" && (
+              <NavLink to="/main" className="select-none">
+                메인
+              </NavLink>
+            )}
           </div>
         </Toolbar>
       </AppBar>
       <NoticeSnackbar />
       <Routes>
-        <Route path="/main" element={<MainPage
-        />}/>
-        <Route path="/write" element={<WritePage
-        />}/>
-        <Route path="*" element={<Navigate to="/main"/>}/>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
     </>
   );
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,36 +1,51 @@
 import { TextField, Button } from "@mui/material";
+import { useTodosStatus } from "../hooks";
 
 export default function WritePage() {
+  const todosStatus = useTodosStatus();
   const onSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    if(form.regDate.value.length == 0) {
+    if (form.regDate.value.length == 0) {
       alert("날짜를 입력해주세요.");
       form.regDate.focus();
       return;
     }
 
-    if(form.content.value.length == 0) {
+    if (form.content.value.length == 0) {
       alert("할 일을 입력해주세요.");
       form.content.focus();
       return;
     }
-  }
-    return (
-      <>
-      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit} >  
-           {/* textfield는 <div><label></label></div><div><input></input></div> 으로 됨 */}
-          <TextField label="언제 해야하나요?" focused type="datetime-local" name="regDate" />
-          <TextField label="무엇을 해야하나요?" className="flex flex-1" InputProps={{className:"flex-1 flex-col"}} inputProps={{className:"flex-1"}} multiline name="content"/>        
-          <Button type="submit" variant="contained">
-            <span>
+
+    todosStatus.addTodo(form.regDate.value, form.content.value);
+  };
+  return (
+    <>
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
+        {/* textfield는 <div><label></label></div><div><input></input></div> 으로 됨 */}
+        <TextField
+          label="언제 해야하나요?"
+          focused
+          type="datetime-local"
+          name="regDate"
+        />
+        <TextField
+          label="무엇을 해야하나요?"
+          className="flex flex-1"
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1" }}
+          multiline
+          name="content"
+        />
+        <Button type="submit" variant="contained">
+          <span>
             <i className="fa-solid fa-pen"></i>
-            </span>
-            <span>&nbsp;</span>
-            <span>할 일 추가</span>
-          </Button>
+          </span>
+          <span>&nbsp;</span>
+          <span>할 일 추가</span>
+        </Button>
       </form>
-      </>
-    );
-  }
-  
+    </>
+  );
+}
