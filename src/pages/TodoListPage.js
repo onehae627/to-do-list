0ffd959f124e784cd998c7a7c974b99ccd5b234f
onehae627 <onehae627@gmail.com>
2023-02-15 17:25:58 +0900

    할 일 리스트, 완료여부 필터링 탭메뉴

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,16 +1,57 @@
 import { useTodosStatus } from "../hooks";
+import { useRecoilState } from "recoil";
+import { TodoList__filterCompletedIndexAtom } from "../atoms";
 import { useTodoOptionDrawerStatus } from "../hooks";
 import TodoOptionDrawer from "../components/TodoOptionDrawer";
 import TodoListItem from "../components/TodoListItem";
+import { Tabs, Tab } from "@mui/material";
 
 export default function TodoListPage() {
   const todosStatus = useTodosStatus();
   const todoOptionDrawerStatus = useTodoOptionDrawerStatus();
   const onCompletedBtnClicked = (id) => todosStatus.toggleTodoCompletedById(id);
 
+  const [filterCompletedIndex, setFilterCompletedIndex] = useRecoilState(
+    TodoList__filterCompletedIndexAtom
+  );
+
   return (
     <>
       <TodoOptionDrawer status={todoOptionDrawerStatus} />
+
+      <Tabs
+        variant="fullWidth"
+        value={filterCompletedIndex}
+        onChange={(event, newValue) => setFilterCompletedIndex(newValue)}
+      >
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-solid fa-list-ul"></i>
+              <span className="ml-2">전체</span>
+            </span>
+          }
+          value={0}
+        />
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-regular fa-square"></i>
+              <span className="ml-2">미완료</span>
+            </span>
+          }
+          value={1}
+        />
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-regular fa-square-check"></i>
+              <span className="ml-2">완료</span>
+            </span>
+          }
+          value={2}
+        />
+      </Tabs>
       <div className="mt-4 px-4">
         <ul>
           {todosStatus.todos.map((todo, index) => (
